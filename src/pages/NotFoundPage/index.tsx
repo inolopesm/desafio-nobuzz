ceef2094663917ee53fedee4bb0ae0cdeda240fc
Matheus Lopes <inolopesm@gmail.com>
2023-11-09 14:56:18 -0300

    feat: style not found page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,6 +1,8 @@
+import styles from "./styles.module.css";
+
 export default function NotFoundPage() {
   return (
-    <div>
+    <div className={styles.container}>
       <div>Página não encontrada</div>
     </div>
   );
